4432989a0bf7b338991e5d7ec4e3b622cc50d9bd
Roberto Oliveira Jr <rloliveirajr@gmail.com>
2015-08-14 13:49:33 -0300

    Init file

--- trans4mers/feature_extraction/__init__.py
+++ trans4mers/feature_extraction/__init__.py
@@ -2,3 +2,4 @@ from .hyperbolic_location_fingerprint import HyperbolicLocationFingerprint
 from .relative_location_fingerprint import RelativeLocationFingerprint
 from .fingerprint import Fingerprint
 from .diff import Diff
+from .ssd import SSD
